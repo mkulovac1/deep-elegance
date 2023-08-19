621df99e08e1fe7f42e0767934476fa118e51625
Merim Kulovac <92273186+mkulovac1@users.noreply.github.com>
2023-08-19 12:25:24 +0200

    created introduction text, customize button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation,
+    fadeAnimation
+
+} from '../config/motion'
+
+import { CustomButton } from '../components'
 
 const Home = () => {
-  return (
-    <div>home</div>
-  )
+
+    const snap = useSnapshot(state) // very useful to quick catch changes on page
+
+    // intro is main page (home page)
+
+    return (
+        <AnimatePresence>
+            {snap.intro && (
+                <motion.section className="home" {...slideAnimation("left")}>
+                    <motion.header {...slideAnimation("down")}>
+                        <img src='./threejs.png' alt="logo" className='w-8 h-8 object-contain'/>
+                    </motion.header>
+
+                    <motion.div className="home-content" {...headContainerAnimation}>
+                        <motion.div {...headTextAnimation}>
+                            <h1 className='head-text'>
+                                LET'S <br className="xl:block hidden"/> DO IT. 
+                            </h1>        
+                        </motion.div>
+
+                        <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+                            <p className='max-w-md font-normal text-gray-600 text-base'>
+                                Create your unique and exclusive t-shirt with this 3D customization tool. 
+                                <strong> Unleash your imagination </strong> {" "} and define your own style.
+                            </p>
+
+                            <CustomButton 
+                                type="filled"
+                                title="Customize it"
+                                handleClick={() => state.intro = false}
+                                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                            />
+                        </motion.div>
+                    </motion.div>
+                </motion.section>
+            )}
+        </AnimatePresence>
+    )
 }
 
 export default Home
---
+++ client/src/store/index.jsx
@@ -0,0 +1,12 @@
+import { proxy } from 'valtio';
+
+const store = proxy({
+    intro: true,
+    color: '#EFBD48',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png',
+})
+
+export default store
